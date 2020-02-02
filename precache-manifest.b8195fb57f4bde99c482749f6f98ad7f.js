384257032a85c069ac35705c09af7fe3e760e2b7
RyosukeOikawa <oikawaryosuke@gmail.com>
2020-02-02 21:06:43 +0900

    Updates

---
+++ precache-manifest.b8195fb57f4bde99c482749f6f98ad7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff289f51df9865b6bc05e433822f8584",
+    "url": "/react-govtech-list/index.html"
+  },
+  {
+    "revision": "84129915d29c409ae845",
+    "url": "/react-govtech-list/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ce1749e38e098d862b39",
+    "url": "/react-govtech-list/static/js/2.4a0f0661.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-govtech-list/static/js/2.4a0f0661.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84129915d29c409ae845",
+    "url": "/react-govtech-list/static/js/main.7e4c810c.chunk.js"
+  },
+  {
+    "revision": "6b8bdad9823de321f90c",
+    "url": "/react-govtech-list/static/js/runtime-main.c677906b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-govtech-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
